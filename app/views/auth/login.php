f00da3a6151547e707bd6030389f457bc3d3de1d
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2023-08-26 23:21:52 +0700

    login and register

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -6,7 +6,13 @@
 </head>
 
 <body>
-	<!-- Login form HTML -->
+	<h1>Login</h1>
+	<form action="index.php?controller=Auth&action=login" method="post">
+		<input type="text" name="username" placeholder="Username" required><br>
+		<input type="password" name="password" placeholder="Password" required><br>
+		<input type="hidden" name="action" value="login">
+		<button type="submit">Login</button>
+	</form>
 </body>
 
 </html>
--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -6,7 +6,13 @@
 </head>
 
 <body>
-	<!-- Registration form HTML -->
+	<h1>Register</h1>
+	<form action="index.php?controller=Auth&action=register" method="post">
+		<input type="text" name="username" placeholder="Username" required><br>
+		<input type="password" name="password" placeholder="Password" required><br>
+		<input type="hidden" name="action" value="register">
+		<button type="submit">Register</button>
+	</form>
 </body>
 
 </html>
