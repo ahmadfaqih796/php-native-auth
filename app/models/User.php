4fa9db3aa5033349866da14ed9eb8912d7ad5306
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2023-08-26 23:20:35 +0700

    user model

--- app/models/User.php
+++ app/models/User.php
@@ -1,5 +1,30 @@
 <?php
+require_once('../config/config.php');
+
 class User
 {
-	// Define user model properties and methods
+	protected $conn;
+
+	public function __construct()
+	{
+		$this->conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+	}
+
+	public function createUser($username, $password)
+	{
+		$query = "INSERT INTO users (username, password) VALUES (:username, :password)";
+		$stmt = $this->conn->prepare($query);
+		$stmt->bindParam(':username', $username);
+		$stmt->bindParam(':password', $password);
+		$stmt->execute();
+	}
+
+	public function getUserByUsername($username)
+	{
+		$query = "SELECT * FROM users WHERE username = :username";
+		$stmt = $this->conn->prepare($query);
+		$stmt->bindParam(':username', $username);
+		$stmt->execute();
+		return $stmt->fetch(PDO::FETCH_ASSOC);
+	}
 }
